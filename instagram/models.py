2e3c19f204744524fe6e0ae6d94f532a9f562f0e
Jose Andrade <nirvajara@hotmail.com>
2017-09-27 11:50:31 -0500

    some prints for tests

--- instagram/models.py
+++ instagram/models.py
@@ -84,6 +84,8 @@ class Media(ApiModel):
         if new_media.type == 'video':
             new_media.videos = {}
             for version, version_info in six.iteritems(entry['videos']):
+                print '#########'*80
+                print version_info
                 new_media.videos[version] = Video.object_from_dictionary(version_info)
 
         if 'user_has_liked' in entry:
